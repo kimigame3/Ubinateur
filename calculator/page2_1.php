3417e99c16123dbc8adbf56ab582cc0b997f0f37
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2022-04-11 16:47:44 +0200

    eventListener sur les checkboxes

--- calculator/page2_1.php
+++ calculator/page2_1.php
@@ -68,6 +68,10 @@
 
     <h1 id="p2_1">e-commerce</h1>
 
+    <p id="affichageValeurs">
+
+    </p>
+
     <form action="" method="post">
         
         <div class="coches">
@@ -133,5 +137,32 @@
 
     </form>
     
+    <script>
+        // à mettre dans un fichier JS
+
+        let boxes = document.querySelectorAll("input[type='checkbox']") // On récupère toutes les checkboxes
+        let values = []; // On crée un tableau pour les valeurs cochées
+
+        // Pour chaque checkbox:
+        boxes.forEach(b =>
+        {
+            // On ajoute un event quand elle change d'état (check / unchecked)
+            b.addEventListener("change", (element) =>
+            {
+                let value = element.target.value; // Le nouvel état de la checkbox
+                let index = values.indexOf(value); // on vérifie l'emplacement de la valeur dans le tableau
+
+                if (index != -1) // si la valeur a un emplacement dans le tableau
+                    values.splice(index, 1); // on la supprime
+                else
+                    values.push(value); // sinon on l'ajoute
+
+                affichageValeurs.innerHTML = values.join(","); // on affiche les valeurs du tableau dans le <p> avec l'id affichageValeurs
+            })
+        });
+    
+    
+    </script>
+
 </body>
 </html>
